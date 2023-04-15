3c77fd4265757b7744ebfa915db1da9e97a9d86c
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-15 19:15:24 +0200

    commenti

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ let annulla = document.getElementById("annulla")
 // var prezzo = km.value * prezzoAlKm
 // let hidden = document.getElementById("hidden-section")
 
-
+// genero un evento al click di stampa
 stampa.addEventListener("click",
 function()
     {
+        // dichiaro e do valore alle variabili
         let km = document.getElementById("km").value;
         let eta = document.getElementById("eta").value;
         let prezzoHtml = document.getElementById("prezzoHtml")
@@ -29,8 +30,10 @@ function()
         let nomePasseggero = document.getElementById("nome-passeggero")
         let hidden = document.getElementById("hidden-section")
 
+        // controllo che i campi siano tutti compila
         if ((km | nome)!= ""){
 
+            // se è minorenne allora genero prezzo - sconto cp ecc
             if (eta == "minorenne"){
                 prezzo = (prezzo - (prezzo * 0.2))
                 prezzoHtml.innerHTML = `${prezzo.toFixed(2)} euro`
@@ -39,7 +42,7 @@ function()
                 offerta.innerHTML = `offerta per minori`
 
             }
-
+            // se è over 60 allora genero prezzo - sconto cp ecc
             else if (eta == "over60"){
                 prezzo = (prezzo - (prezzo * 0.2))
                 prezzoHtml.innerHTML = `${prezzo.toFixed(2)} euro`
@@ -47,7 +50,7 @@ function()
                 codiceCp.innerHTML = Math.floor(Math.random()*350 + 1)
                 offerta.innerHTML = `offerta senior`
             }
-
+            // se è maggiorenne allora genero prezzo - sconto cp ecc
             else {
                 prezzo = prezzo 
                 prezzoHtml.innerHTML = `${prezzo.toFixed(2)} euro`
@@ -55,21 +58,18 @@ function()
                 codiceCp.innerHTML = Math.floor(Math.random()*350 + 1)
                 offerta.innerHTML = `offerta standard`
             }
-
             nomePasseggero.innerHTML = `${nome}`
+            // faccio in modo che la seconda section si veda al click
             hidden.style.display='block'
         }
-
+        // se i dati non sono compilati crea un alert
         else {
             alert("inserire tutti i dati richiesti")
         }
 
     }
-
-    
-
 )
-
+// un bottone che elimina i valori precedenti inseriti
 annulla.addEventListener("click",
 
     function(){
